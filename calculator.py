f234a6610d224dfb39ca2b7078b24a070dfd7c46
jngabbard <jngabbard@gmail.com>
2025-11-18 18:09:12 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,36 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
+
+
+#################### Partner 1 Functions Start ####################
+
+def
+
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Error: cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 1 or b <= 0:
+        raise ValueError("Error: invalid domain for log function")
+    return log(b, a)
+
+def exp(a, b):
+    a ** b
+
+#################### Partner 1 Functions End ####################
